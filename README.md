66316f330cf28959e613aca0d1e123787ae87ec5
Andre Dewes <andredewes@microsoft.com>
2024-03-28 22:55:52 -0300

    Updating readme

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
 # Useful HTTP scripts to manage Azure AI Search
 
-This repository aims to provide some HTTP files (which you can use in Visual Studio Code) to quickly performs operations in Azure AI Search that are not exposed in the Azure Portal interface.
+This repository aims to provide some Python script files and functions (which you can use in Visual Studio Code) to quickly performs operations in Azure AI Search that are not exposed in the Azure Portal interface or to overcome problems related to private networking scenarios.
 
 ## Pre-requisites
 
-- Visual Studio code with [REST Client](https://github.com/Huachao/vscode-restclient) extension installed 
-- [Azure CLI](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli)
+- Visual Studio Code Phyton extensions
 
 ## Usage
 
-You can just open this repository in Visual Studio Code, find the script you want, and before running the requests make sure to replace all the variables in the beginning of the file (they start with ``@``) to fit your scenario.
+You can just open this repository in Visual Studio Code, find the script you want, and before running the requests make sure to replace all the variables in the beginning of the file to fit your scenario.
 
-Pay attention to the ``@azureAccessToken`` variable. To acquire an Azure access token to manage your AI Search instance, you need to run this AZ CLI command:
+Pay attention to the ``azureAccessToken`` variable. To acquire an Azure access token to manage your AI Search instance, you need to run this AZ CLI command:
 
 ``az account get-access-token --scope https://search.azure.com/.default --subscription <<your-subscription-id>>``
 
@@ -21,5 +20,5 @@ Then copy the ``accessToken`` value from the output. It should begins with ``ey.
 
 | Script                            | Description   |
 | ---                               | ---           | 
-| integrated-vectorization.http     |  This script replicates the same steps that happens when you run the [Integrated Vectorization](https://learn.microsoft.com/en-us/azure/search/search-get-started-portal-import-vectors) wizard. It is useful when you disable Public Access from the networking blade because the button from the portal is disabled in that scenario. It creates a Data Source, Index, Skillset and Indexer |
+| integrated-vectorization-default.py     |  This script replicates the same steps that happens when you run the [Integrated Vectorization](https://learn.microsoft.com/en-us/azure/search/search-get-started-portal-import-vectors) wizard. It is useful when you disable Public Access from the networking blade because the button from the portal is disabled in that scenario. It creates a Data Source, Index, Skillset and Indexer |
 | /azure-vision-ocr     |  This folder contains a Azure Function (Python 3.11) that will receive images from an AI Search skillset and submit them to Azure Vision OCR endpoint. This replaces the built-in OCR skill for scenarios where AI Search is in private networking (built-in AI skills don't work) |
--- azure-vision-ocr/function/function_app.py
+++ azure-vision-ocr/function/function_app.py
@@ -1,95 +1,66 @@
-import base64
-import json
-import os
 import azure.functions as func
-import requests
+import logging
+import json
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-ai_multiservice_endpoint = os.getenv("AI_MULTI_SERVICE_ENDPOINT")
-ai_multiservice_key = os.getenv("AI_MULTI_SERVICE_KEY")
+@app.route(route="get_folder")
+def get_folder(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP get_folder function processed a request.')
 
-if (ai_multiservice_endpoint is None or ai_multiservice_key is None):
-    raise Exception("Please provide the AI_MULTI_SERVICE_ENDPOINT and AI_MULTI_SERVICE_KEY environment variables.")
-
-#The incoming request from AI Search WebApi will be in the following format: https://learn.microsoft.com/en-us/azure/search/cognitive-search-custom-skill-web-api#sample-input-json-structure
-#The response from the function should be in the following format: https://learn.microsoft.com/en-us/azure/search/cognitive-search-custom-skill-web-api#sample-output-json-structure
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    # Custom Web API skill JSON reference
+    # https://learn.microsoft.com/en-us/azure/search/cognitive-search-custom-skill-web-api
 
     try:
-        body = json.dumps(req.get_json())
+        logging.debug(f"Received RAW Body: {req.get_body()}")
+        rjson = json.loads(req.get_body())
+        logging.debug(f"JSON body loaded")
 
-        if body:
-            result = compose_response(body)
-
-            return func.HttpResponse(result, mimetype="application/json")
-        else:
-            return func.HttpResponse(
-                "Invalid body",
-                status_code=400
-            )
-    except ValueError:
+        for key in rjson['values']:
+            logging.debug(f"Key in values array: {key}")
+            url = key['data']['url']
+            logging.info(f"Received URL: {url} in recordId: {key['recordId']}")
+            folder = url.split("/")[-2]
+            del key['data']['url']
+            logging.debug(f"URL removed from recordId: {key['recordId']}")
+            key['data']['folder'] = folder
+            logging.debug(f"Folder added to recordId: {key['recordId']}")
+        response = json.dumps(rjson)
+        logging.info(f"Response: {response}")
         return func.HttpResponse(
-            "Invalid body",
-            status_code=400
+            f"{response}",
+            mimetype="application/json"
         )
 
-def compose_response(json_data):
-    values = json.loads(json_data)['values']
-    
-    # Prepare the Output before the loop
-    results = {}
-    results["values"] = []
-    
-    for value in values:
-        output_record = analyze_image(endpoint=ai_multiservice_endpoint, key=ai_multiservice_key, recordId=value["recordId"], data=value["data"])        
-        results["values"].append(output_record)
+    except json.decoder.JSONDecodeError as errorMessage:
+        responseJson={}
+        responseJson['values']=[]
+        responseJson['values'].append({
+            "recordId": "0",
+            "data": {},
+            "errors": None,
+            "warnings": None
+        })
+        responseJson['values'][0]['errors']=[]
+        responseJson['values'][0]['errors'].append({"message": str("HTTP request does not contain valid JSON data. Exception: "+str(errorMessage))})
+        response = json.dumps(responseJson)
+        logging.error(f"Error: {str(response)}")
+        return func.HttpResponse(
+             f"{response}",
+             status_code=200
+        )
 
-    return json.dumps(results, ensure_ascii=False)
+    except IndexError as errorMessage:
+        rjson = json.loads(req.get_body())
+        for key in rjson['values']:
+            logging.info(f"Key: {key}")
+            del key['data']['url']
+            key['data']['errors'] = []
+            key['data']['errors'].append({"message": str("One of the URLs doesn't have a valid URI format. Exception: "+str(errorMessage))})
+        response = json.dumps(rjson)
+        logging.error(f"Error: {str(response)}")
 
-def analyze_image(endpoint, key, recordId, data):
-    try:
-        imageAsBase64 = data["imageData"]
-
-        headers = {
-            'Content-Type': 'application/octet-stream',
-            'Ocp-Apim-Subscription-Key': key,
-        }
-
-        base64Bytes = imageAsBase64.encode('utf-8')
-        inputBytes = base64.b64decode(base64Bytes)
-
-        response = requests.post(f'{endpoint}/vision/v3.2/ocr', headers=headers, data=inputBytes)
-
-        output_record = {}
-
-        if response.status_code == 200 or response.status_code == 201:
-            line_infos = [region["lines"] for region in response.json()["regions"]]
-            words= []
-            for line in line_infos:
-                for word_metadata in line:
-                    for word_info in word_metadata["words"]:
-                        words.append(word_info)
-
-            finalText = ' '.join([word["text"] for word in words])
-
-            output_record = {
-                "recordId": recordId,
-                "data": {
-                    "text": finalText
-                },
-                "errors": [],
-                "warnings": []
-            }
-
-        else:
-            raise Exception(response.text)
-        
-    except Exception as error:
-        output_record = {
-            "recordId": recordId,
-            "errors": [ { "message": "Error: " + str(error) }   ] 
-        }
-
-    return output_record
+        return func.HttpResponse(
+             f"{response}",
+             status_code=200
+        )
